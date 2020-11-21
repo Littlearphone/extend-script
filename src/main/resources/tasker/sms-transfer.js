8e6e6e4a61fe2ce63e7012b4743ee791fc90af21
NUC <littlearphone@outlook.com>
2020-11-21 22:00:14 +0800

    Adjust baidu page layout; Preserve tts tasker script.

---
+++ src/main/resources/tasker/sms-transfer.js
@@ -0,0 +1,52 @@
+//下面的三个变量值需要修改
+var ID = "ww4719b360fb222840";
+var SECRET = "rp85qwoarw_UeVzmrTtA38o9_rwiFe8cYt3B4q9vvfw";
+var AGENTID = "1000002";
+
+//定义post方法
+function posthttp(url, data) {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            flash(this.responseText); //显示返回消息,可删除本行
+        }
+    });
+    xhr.open("POST", url, false);
+    xhr.send(data);
+    return xhr.responseText;
+}
+
+//定义get方法
+function gethttp(url) {
+    var xhr = new XMLHttpRequest();
+    xhr.addEventListener("readystatechange", function () {
+        if (this.readyState === 4) {
+            flash(this.responseText); //显示返回消息,可删除本行
+        }
+    });
+    xhr.open("GET", url, false);
+    xhr.send();
+    return xhr.responseText;
+}
+
+//获取token
+var gettoken = "https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=" + ID + "&corpsecret=" + SECRET;
+var ACCESS_TOKEN = JSON.parse(gethttp(gettoken)).access_token;
+
+//发送消息(文本)
+var SMSRF = global('SMSRF');
+var SMSRB = global('SMSRB');
+var SMSRT = global('SMSRT');
+var SMSRD = global('SMSRD');
+var CONTENT = "在" + SMSRD +  " " + SMSRT +"\n收到" + SMSRF +"的短信：\n" + SMSRB;
+var message = JSON.stringify({
+    "touser": "@all",
+    "msgtype": "text",
+    "agentid": AGENTID,
+    "text": {
+        "content": CONTENT
+    },
+    "safe": 0
+});
+var send = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=" + ACCESS_TOKEN;
+posthttp(send, message);
--- src/main/resources/tasker/sms-tts.js
+++ src/main/resources/tasker/sms-tts.js
@@ -1,3 +1,6 @@
+// 似乎新版安卓下获取不到短信，所以使用通知读取内容
+// const SMSRN = global('SMSRN');
+// const SMSRB = global('SMSRB');
 var name = evtprm[1];
 var body = evtprm[2];
 var ttscontent = '';
@@ -9,3 +12,4 @@ if (extract && extract.length > 1) {
     ttscontent = '收到短信验证码：' + extract[1];
 }
 // js(let)后面再建一条"文本转语音"的操作，直接播出％ttscontent
+// 语音引擎可以在辅助功能里面打开tts，使用默认的google引擎就可以
